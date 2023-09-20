b0f22c97d8af337b7e269ce6fd763ee690b1afc3
Justin <jbcool02@yahoo.com>
2023-09-19 22:16:49 -0500

    yaml file added

--- WebScraper/main.py
+++ WebScraper/main.py
@@ -3,14 +3,18 @@
 import sys
 import requests
 
-
+#NOTE: CLEAR THE 'content.txt' FILE TO SEE THE PROGRAM EXTRACT FROM AN EMPTY STATE.
+#'To Run Program -> python .\main.py *URL*
+#    ---->          python  your_program.py  URL-Link
 URL = sys.argv[1]
 
 file = open("content.txt", "w")
 page_to_scrape = requests.get(URL)
-
+print("Page Scrape started...")
 
 file.write(page_to_scrape.text)
+print("Page extracted...")
+
 
 file.close()
 print("Scraper Ran ...")
